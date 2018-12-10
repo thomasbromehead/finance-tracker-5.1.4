692e2603be77df57aee3e6bc04cc5fc4771662c6
Thomas Bromehead <t_bromehead@yahoo.fr>
2018-12-10 11:48:33 +0100

    feat(Errors) Add Error Handling, Flash Message and Begin/Rescue on the model :ok_hand:

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -7,13 +7,10 @@ class Stock < ApplicationRecord
   end
   
   def self.new_from_lookup(ticker_symbol)
-    begin 
+    begin
       looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
-      
-    
-      price = strip_commas(looked_up_stock.l)
-      new(name: looked_up_stock.name, ticker: looked_up_stock.symbol, last_price: price)
-    rescue Exception => e 
+      new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol, last_price: looked_up_stock.latest_price)
+    rescue Exception => e
       return nil
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :registrations => "user/registrations" }
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
+  get 'search_stocks', to: 'stocks#search'
 
-  
+
 end
