501cd3d5d42b99b939bcb60d89943f9d1eeb5f11
Thomas Bromehead <t_bromehead@yahoo.fr>
2018-12-10 17:02:56 +0100

    feat(Friendship): Add Friendship model and aliases for Users :sparkles:

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,7 @@
 class Friendship < ApplicationRecord
   belongs_to :user
-  belongs_to :friend, :class_name => 'User'
+  #Create a friend alias for a user
+  #Set optional to true if you want to be able to create the Experience w/o a winning_order which will be determined later on.
+  belongs_to :friend, class_name: 'User', optional: true
+  
 end
