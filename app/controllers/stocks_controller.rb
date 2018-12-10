b75edc37bebef2229d4691023b389c6f31779627
Thomas Bromehead <t_bromehead@yahoo.fr>
2018-12-10 13:59:36 +0100

    fix(Stock Controller): Refactor Search Method :recycle:

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,21 +1,18 @@
 class StocksController < ApplicationController
 
   def search
-    if params[:stock].present?
-      @stock = Stock.new_from_lookup(params[:stock])
-      if @stock
-        respond_to do |format|
-          format.js { render partial: "users/result"}
-          format.html { render 'users/my_portfolio'}
-        end
-      else
-        flash[:danger] = "You've entered an incorrect symbol"
-        render 'users/my_portfolio'
-      end
+    if params[:stock].blank?
+      flash.now[:danger] = "You've entered an empty search string"
     else
-      flash[:danger] = "You've entered an empty search string"
-      redirect_to my_portfolio_path
+      @stock = Stock.new_from_lookup(params[:stock])
+      flash.now[:danger] = "You've entered an incorrect symbol" unless @stock
     end
+
+    respond_to do |format|
+      format.js { render partial: "users/result" }
+      format.html { render 'users/my_portfolio' }
+    end    
+
   end
 
 end
